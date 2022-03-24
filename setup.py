dafd260656a728afad648d4cb72944eafaa17442
brosscle <clement.brossard@univ-grenoble-alpes.fr>
2022-03-24 21:16:55 +0100

    update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/",
     packages=['CT_TIQUA', 'CT_TIQUA.blast_ct', 'CT_TIQUA.blast_ct.blast_ct', 'CT_TIQUA.blast_ct.blast_ct.trainer', 'CT_TIQUA.blast_ct.blast_ct.nifti', 'CT_TIQUA.blast_ct.blast_ct.models'],
     package_data={'': ['data/*', 'README.md', 'data/saved_models/*.pt', 'data/config.json', 'data/Labels_With_0.csv', 'data/Resliced_Registered_Labels_mod.nii.gz', 'data/TEMPLATE_miplab-ncct_sym_brain.nii.gz']},
-    scripts=['CT_TIQUA.python_scripts/Volume_Estimation.py'],
+    scripts=['CT_TIQUA/python_scripts/Volume_Estimation.py'],
     entry_points={
         'console_scripts': [
             'ct-tiqua = CT_TIQUA.console_tool:console_tool',
